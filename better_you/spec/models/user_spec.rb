ad1e01834d5b6d48a33f5ffa4c3cd0fb4ea277f5
ericswetts <eric.swetts@gmail.com>
2018-11-17 08:50:30 -0800

    Build out User model. Struggle heartily with validations

--- better_you/spec/models/user_spec.rb
+++ better_you/spec/models/user_spec.rb
@@ -1,19 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
 
-  describe "Test User class" do
 
-    subject(:user) {User.new(username:'first_user',
-      password_hash:"gdfhwhbtrn",
-      session_token:"testtetsttest"
-      )}
-    context "Verify new user has all fields"
+      subject(:user) do
+        user = User.new(username:nil, password_hash: nil)
+        user.password=('password')
+        user
+      end
+
 
       it {should validate_presence_of(:username)}
       it {should validate_presence_of(:password_hash)}
-      it {should validate_presence_of(:session_token)}
+      it {should validate_length_of(:password).is_at_least(6)}
+      it {should have_many(:goals) }
+
     end
 
+    it "encrypts the password using BCrypt" do
+    expect(BCrypt::Password).to receive(:create)
+    User.new(username: "mary_mack", password: "abcdef")
 end
+
+    # describe "Creating new user" do
+    #
+    #   subject(:user2) do
+    #     User.create(:username:"second_user",
+    #     password_hash:"hdbvioewobfewbr",
+    #     session_token: "1234564322")
+    #     User.find_by(username:"second_user")
+    #   end
+    #
+    # it {should validate_presence_of(:password_hash)}
+    # it {should validate_presence_of(:session_token)}
+    # end
